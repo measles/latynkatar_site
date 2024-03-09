d314cdcda7908fe657f727f0fa788b9ccb87055d
Andrej Zacharevicz <andrej@zahar.ws>
2024-03-09 13:48:42 +0100

    Дадаў праверку што уваходны тэкст існуе перад тым як дасылаць запыт на бэкэнд closes #16

--- static/script.js
+++ static/script.js
@@ -1,22 +1,26 @@
 function getConverted(){
     let inputField = document.getElementById("input");
     let outputField = document.getElementById("output");
-    fetch('/convert', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "text": inputField.value,
-        "operation": "convertation",
-        "direction": "latin"
-    })
-})
-    .then(response => response.json())
-   //.then(response => console.log(JSON.stringify(response["text"])))
-    .then(response => outputField.value = response["text"])
-    //document.getElementById("output").value = inputField.value;
+    if (inputField.value.length > 0) {
+        fetch('/convert', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "text": inputField.value,
+                "operation": "convertation",
+                "direction": "latin"
+            })
+        })
+        .then(response => response.json())
+        //.then(response => console.log(JSON.stringify(response["text"])))
+        .then(response => outputField.value = response["text"])
+        //document.getElementById("output").value = inputField.value;
+    } else {
+        console.log("The input field was empty. Skipping backend request")
+    }
 }
 
 function resetInput(){
