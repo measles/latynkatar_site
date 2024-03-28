91f26682d424dbb6b9116e5918d247b81bd53c00
Andrej Zacharevicz <andrej@zahar.ws>
2024-03-29 02:27:30 +0300

    Дадаў старонку з праверкай на наяўнасць усіх літар беларускага алфавіту ў тэксце (#31)

--- static/script.js
+++ static/script.js
@@ -94,3 +94,23 @@ function showHideHelp(){
         toastBootstrap.show()
     }
 }
+
+function checkLetters() {
+    const alfavit = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'і', "й",	'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ў', 'ф', 'х', 'ц', 'ч', 'ш', 'ы',  'ь', 'э', 'ю', 'я'];
+    const text = document.getElementById("input").value.toLowerCase();
+    let notFound = []
+    let message = ""
+
+
+    for (char of alfavit) {
+        if  (!text.includes(char)) {
+            notFound.push(char)
+        }
+    }
+    if (notFound.length === 0) {
+        message = "Усе літары знойдзены"
+    } else {
+        message = "Няма літар: " + notFound.toString()
+    }
+    document.getElementById("vyniki").innerHTML = message
+}
